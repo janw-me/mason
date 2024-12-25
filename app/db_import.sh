7fd309355d7049a5eb4c1bcdf37f323c29769145
Jan-Willem Oostendorp <webmaster@janw.me>
2024-12-25 14:00:00 +0100

    Added fix for wonky export file.

--- app/db_import.sh
+++ app/db_import.sh
@@ -6,6 +6,12 @@ function db_import() {
   wp @live db dump --exclude_tables=${PREFIX}toolset_maps_address_cache - > dump.sql
   #------------------------------------------------------------------------^ Formatting will strip this space, keep it!!
   echo 'Importing DB'
+
+  # Strip sandbox mode it's at the top of the dump.
+  sed '1{/\/\*M!999999\\- enable the sandbox mode \*\//d}' dump.sql > dump-stripped.sql
+  if [[ -f dump-stripped.sql ]]; then
+    mv dump-stripped.sql dump.sql
+  fi
   wp db reset --yes
   wp db import dump.sql
   # cleanup
